42b0d4a6863f0031add0daea015049cda4b02621
Fidelis Nwobuwa Ololube <fn_ololube@yahoo.com>
2016-10-22 19:11:36 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var ip   = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('public'));
+var socket = require('socket.io');
+
+io.sockets.on('connection', newConnection);
+
+	
+var users = [];
+var userIds = [];
+
+function newConnection(socket){
+  	console.log('new connection: ' + socket.id);
+
+	socket.emit('myId', users.length);
+	userIds[users.length] = socket.id;
+
+	users.push(users.length);	
+	io.emit('friends', users);
+	socket.on('updateUser', function(data){
+		socket.broadcast.emit('updateUser', data);
+	});
+
+	socket.on('disconnect', function(){
+	  var deleting = userIds.indexOf(socket.id);        
+          userIds.splice(deleting,1);
+          users.splice(deleting,1);
+          io.emit('remove-user', {detail: deleting});        
+        });	
+};
+http.listen(port,ip);
