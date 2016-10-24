34119adfaf68804894467c403bd02ef9c8e3e3ab
fidelis <fn_ololube@yahoo.com>
2016-10-24 13:05:29 +0300

    edited index with promise

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-//var ip   = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+//var ip   = process.env.IP || '127.0.0.1';
 var port = process.env.PORT || 3000;
 
 var http = require('http').Server(app);
@@ -23,15 +23,17 @@ function newConnection(socket){
 
 	users.push(users.length);	
 	io.emit('friends', users);
+
 	socket.on('updateUser', function(data){
-		socket.broadcast.emit('updateUser', data);
+		socket.broadcast.emit('updateAllUser', data);
 	});
 
 	socket.on('disconnect', function(){
 	  var deleting = userIds.indexOf(socket.id);        
-          userIds.splice(deleting,1);
-          users.splice(deleting,1);
-          io.emit('remove-user', {detail: deleting});        
-        });	
+      userIds.splice(deleting,1);
+      users.splice(deleting,1);
+      console.log(socket.id +' deleted');
+      io.emit('remove-user', {detail: deleting});             
+    });	
 };
-http.listen(port);
+http.listen(port); //(port,ip)
