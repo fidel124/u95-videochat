875506d99110ec68a3ea94434903cfeae3f1d596
fidelis <fn_ololube@yahoo.com>
2016-10-26 02:20:13 +0300

    editing index and server

--- server.js
+++ server.js
@@ -17,11 +17,14 @@ function newConnection(socket){
   	console.log('new connection: ' + socket.id);
   	socket.emit('myId', socket.id);  	
   	allIds.push(socket.id);  	
-	io.emit('connectIds', allIds);// first
+	  io.emit('connectIds', allIds);// first
 
 
-	socket.on('updateUser', function(data){
-		socket.broadcast.emit('updateAllUser', {theIds:data.streamId, theCaps:data.capture});		
+	socket.on('updateUser', function(data){    
+    //var blobArray = [];    
+    //blobArray.push(data);
+    //io.sockets.emit('updateAllUser', data);    
+		socket.broadcast.emit('updateAllUser', data);		
 	});
 
 	socket.on('disconnect', function(){
