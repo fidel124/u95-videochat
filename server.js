48a323cb723130f61df18408aa058201bae948cc
fidelis <fn_ololube@yahoo.com>
2017-03-15 00:04:42 +0200

    server socket

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 //var ip   = process.env.IP || '127.0.0.1';
 var port = process.env.PORT || 3000;
 
-var http = require('http').Server(app);
+var http = require('http',{forceNew: true}).Server(app);
 var io = require('socket.io')(http);
 
 app.use(express.static(__dirname+'/public'));
@@ -13,26 +13,25 @@ var socket = require('socket.io');
 io.sockets.on('connection', newConnection);
 
 var allIds = [];
-function newConnection(socket){  	
+function newConnection(socket){   
 
   //console.log('new connection: ' + socket.id);
-  socket.emit('myId', socket.id);  	
-  allIds.push(socket.id);  	
-	io.emit('connectIds', allIds);// first
-
+  socket.emit('myId', socket.id);   
+  allIds.push(socket.id);   
+  io.emit('connectIds', allIds);// first  
       
-	socket.on('updateUser', function(data){
-    if(data){     
+  socket.on('updateUser', function(data){       
+      io.sockets.emit('updateAllUser', data);
       //socket.broadcast.emit('updateAllUser', data); 
-      io.sockets.emit('updateAllUser', data);   
-    }    		
-	}); 
+      //io.sockets.emit(''+data.streamId+'', {fid:data.streamId,fdt:data.capture});   
+                    
+  }); 
    
-	socket.on('disconnect', function(){
-	  var deleting = allIds.indexOf(socket.id);        
+  socket.on('disconnect', function(){
+    var deleting = allIds.indexOf(socket.id);        
       allIds.splice(deleting,1);      
       //console.log(socket.id +' deleted');      
       io.emit('remove-user', {detail: socket.id});             
-    });	
+    }); 
 };
 http.listen(port); //(port,ip)
